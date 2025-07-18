f7f8009d099a4c14e93f1ccb8d25617bed2b277b
pavelfedorenko <pavel.fedorenko0602@gmail.com>
2025-07-18 22:23:49 +0700

    delete product_by_id from service

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -10,21 +10,6 @@ async def get_products(session: AsyncSession) -> list[Product]:
     return products
 
 
-async def get_product_by_id(
-    session: AsyncSession,
-    product_id: int,
-) -> Product:
-    product = await crud.get_product_by_id(
-        session=session,
-        product_id=product_id,
-    )
-    if product is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="product not found"
-        )
-    return product
-
-
 async def create_product(
     session: AsyncSession,
     product_in: ProductCreate,
