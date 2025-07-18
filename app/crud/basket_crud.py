69d89b7ace725c386fb595fb5401c4c08ded4a17
pavelfedorenko <pavel.fedorenko0602@gmail.com>
2025-07-18 14:21:36 +0700

    add get_basket_by_user_id in crud

--- app/crud/basket_crud.py
+++ app/crud/basket_crud.py
@@ -1,13 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, Result
+
 from app.core.models.basket import Basket
 
 
-def get_basket_by_user_id() -> Basket | None:
+async def get_basket_by_user_id(user_id: int, session: AsyncSession) -> Basket | None:
+    stmt = select(Basket).where(Basket.user_id == user_id)
+    result: Result = await session.execute(stmt)
+    basket = result.scalar_one_or_none()
+    return basket
+
+
+async def create_basket() -> Basket:
     pass
 
 
-def create_basket() -> Basket:
-    pass
-
-
-def delete_basket() -> None:
+async def delete_basket() -> None:
     pass
