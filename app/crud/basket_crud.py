05bb42519e6bba80e5f970ead6b0d7648326809e
pavelfedorenko <pavel.fedorenko0602@gmail.com>
2025-07-18 14:14:39 +0700

    add base for basket_crud

---
+++ app/crud/basket_crud.py
@@ -0,0 +1,13 @@
+from app.core.models.basket import Basket
+
+
+def get_basket_by_user_id() -> Basket | None:
+    pass
+
+
+def create_basket() -> Basket:
+    pass
+
+
+def delete_basket() -> None:
+    pass
